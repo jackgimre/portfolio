0afec9ad5bb33b77c444d2db1eaf28d71f7b9aca
jackgimre <66290453+jackgimre@users.noreply.github.com>
2025-01-08 18:15:11 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
+      <Routes>
+        <Route path='*' element={<App />}></Route>
+      </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
