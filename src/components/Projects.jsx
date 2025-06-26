42746ef5e6d36856ace7a44988f3939fc88c848d
Jack Gimre <jackgimre@gmail.com>
2025-06-25 21:29:49 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,16 @@
-import React from 'react'
-import Project from './Project'
+import React from 'react';
+import Project from './Project';
 
 import lxr1 from '../assets/LXR/forums.jpg';
 import lxr2 from '../assets/LXR/bibleplans.jpg';
 import lxr3 from '../assets/LXR/profile.jpg';
-import nwp1 from '../assets/NWP/natewafe.JPG';
 const stcbURL = 'https://www.facebook.com/SavetheCowboy/';
-const nwpURL = 'https://www.instagram.com/natewafephotos/';
 const stcbA = <a href={stcbURL} target="_blank" className="text-blue-500 underline hover:no-underline">church in Colorado</a>
-const nwpA = <a href={nwpURL} target='_blank' className="text-blue-500 underline hover:no-underline">Nathan Wafer</a>
 const lxrDesc = <>Long X Ranch Cowboys is a subscription service for a {stcbA}. Providing a forum, interactive bible plans, and daily bible readings, this platform intends to engage the congregation and build stronger connections. User authentication, a secure payment system, input validation, and parsing bible API data are key qualities of this project.</>
-const nwpDesc = <>The Nate Wafe Photos website is a photography portfolio and booking website for {nwpA}. He is a photographer from the Chicagoland area who interacts with clients daily. My website helps clients book shoots faster, organize upcoming shoots, and streamline Nathan's business process.</>
-
 
 const lxrImages = [lxr2, lxr1, lxr3];
-const nwpImages = [nwp1];
 
 const lxrStack = ['MongoDB','Express','NodeJS'];
-const nwpStack = ['MongoDB','Express','React','NodeJS'];
 
 const Projects = () => {
   return (
